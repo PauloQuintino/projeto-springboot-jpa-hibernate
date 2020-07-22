7c4cdda81e76dc3b6066fce49c07c5b907ba83b3
PauloQuintino <pauloqfs16@gmail.com>
2020-07-22 08:34:50 -0300

    Project created

---
+++ src/main/java/com/pauloquintino/projetojpa/ProjetoJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.pauloquintino.projetojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoJpaHibernateApplication.class, args);
+	}
+
+}
