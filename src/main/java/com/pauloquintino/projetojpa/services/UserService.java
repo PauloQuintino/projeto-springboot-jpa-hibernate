7e59f5d095d75ac0088f2775850a5ba7ae8ac773
PauloQuintino <pauloqfs16@gmail.com>
2020-07-28 14:37:14 -0300

    User update

--- src/main/java/com/pauloquintino/projetojpa/services/UserService.java
+++ src/main/java/com/pauloquintino/projetojpa/services/UserService.java
@@ -9,29 +9,46 @@ import org.springframework.stereotype.Service;
 import com.pauloquintino.projetojpa.entities.User;
 import com.pauloquintino.projetojpa.repositories.UserRepository;
 
-@Service //--> registra o UserService como service do Spring para poder ser injetado com o Autowired em outras classes
+@Service // --> registra o UserService como service do Spring para poder ser injetado com
+			// o Autowired em outras classes
 public class UserService {
 
-	//injetando dependencia do User Repository	
+	// injetando dependencia do User Repository
 	@Autowired
 	private UserRepository repository;
-	
-	//metodo para retornar todos os usuários do banco
-	public List<User> findAll(){
+
+	// metodo para retornar todos os usuários do banco
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
-	
+
+	public User update(Long id, User obj) {
+		// pega uma entidade que vai ser monitorada
+		User entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	/* metodo que vai atualizar um objeto de User de acordo com o que foi atualizado
+	* na entidade monitorada */
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+
+	}
+
 }
