be7b6934dd2b80ec1a0ec3d3873d90d5512e2e76
PauloQuintino <pauloqfs16@gmail.com>
2020-07-28 14:58:45 -0300

    Exception handling - findById

---
+++ src/main/java/com/pauloquintino/projetojpa/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.pauloquintino.projetojpa.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.pauloquintino.projetojpa.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	//TRATAMENTOS
+	//metodo que vai interceptar qualquer exeção de ResourceNotFoundException e fazer os tratamentos
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+}
--- src/main/java/com/pauloquintino/projetojpa/services/UserService.java
+++ src/main/java/com/pauloquintino/projetojpa/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.pauloquintino.projetojpa.entities.User;
 import com.pauloquintino.projetojpa.repositories.UserRepository;
+import com.pauloquintino.projetojpa.services.exceptions.ResourceNotFoundException;
 
 @Service // --> registra o UserService como service do Spring para poder ser injetado com
 			// o Autowired em outras classes
@@ -24,7 +25,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
