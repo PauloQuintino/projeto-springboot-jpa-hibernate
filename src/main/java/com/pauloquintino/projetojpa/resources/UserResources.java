b27e54e0a3ae31269b77c7ded4addcc0c633e45a
PauloQuintino <pauloqfs16@gmail.com>
2020-07-28 12:58:47 -0300

    User insert

--- src/main/java/com/pauloquintino/projetojpa/resources/UserResources.java
+++ src/main/java/com/pauloquintino/projetojpa/resources/UserResources.java
@@ -1,13 +1,17 @@
 package com.pauloquintino.projetojpa.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.pauloquintino.projetojpa.entities.User;
 import com.pauloquintino.projetojpa.services.UserService;
@@ -27,11 +31,20 @@ public class UserResources {
 		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id){
+	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
+	// adicionando um novo User através do POST
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		// criando o endereço da requisição POST para inserir um novo usuário
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+
 }
